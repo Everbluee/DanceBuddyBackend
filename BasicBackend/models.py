0608ac857320b0e8b2d398ffe548353b059acf03
Patryk Kasztelan <patryk@ark-software.com>
2024-11-22 15:49:26 +0100

    Repair password hashing when adding user via admin panel

--- BasicBackend/models.py
+++ BasicBackend/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
@@ -59,6 +60,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith(('pbkdf2_sha256$', 'bcrypt$', 'argon2')):
+            self.password = make_password(self.password)
+
+        super().save(*args, **kwargs)
 
 
 class ClassAttendance(BaseAttendance):
